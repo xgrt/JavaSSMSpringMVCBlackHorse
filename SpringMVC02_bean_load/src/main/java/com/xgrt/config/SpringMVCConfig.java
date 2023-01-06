5e2d24cbc4b9759aa1217a7d48061bf17219c66f
xgrt <2725751476@qq.com>
2023-01-06 19:28:12 +0800

    Initial commit

--- SpringMVC02_bean_load/src/main/java/com/xgrt/config/SpringMVCConfig.java
+++ SpringMVC02_bean_load/src/main/java/com/xgrt/config/SpringMVCConfig.java
@@ -1,9 +1,10 @@
 package com.xgrt.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 //3、创建SpringMVC的配置文件，加载controller对应的bean
-//@Configuration
+@Configuration
 @ComponentScan("com.xgrt.controller")//主键扫描，加载对应的bean
 public class SpringMVCConfig {
 }
