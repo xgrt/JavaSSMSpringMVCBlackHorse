57f476f512cd9cb8377ea54542245dfddd4ba3e9
xgrt <2725751476@qq.com>
2023-01-06 18:33:18 +0800

    Initial commit

--- SpringMVC02_bean_load/src/main/java/com/xgrt/config/ServletContainersInitConfig.java
+++ SpringMVC02_bean_load/src/main/java/com/xgrt/config/ServletContainersInitConfig.java
@@ -2,32 +2,49 @@ package com.xgrt.config;
 
 //4、定义一个Servlet容器启动的配置类，在里面加载Spring的配置
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+/*
+* 简化开发
+* */
+public class ServletContainersInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
     @Override
-    //加载SpringMVC容器配置
-    protected WebApplicationContext createServletApplicationContext() {
-        AnnotationConfigWebApplicationContext atx = new AnnotationConfigWebApplicationContext();//初始化一个空的AnnotationConfigWebApplicationContext容器
-        atx.register(SpringMVCConfig.class);//将SpringMVCConfig.class注册到容器中，即加载具体的配置
-        return atx;
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringMVCConfig.class};
     }
 
     @Override
-    //加载Spring容器的配置
-    protected WebApplicationContext createRootApplicationContext() {
-        AnnotationConfigWebApplicationContext atx = new AnnotationConfigWebApplicationContext();//初始化一个空的AnnotationConfigWebApplicationContext容器
-        atx.register(SpringConfig.class);//将SpringMVCConfig.class注册到容器中，即加载具体的配置
-        return atx;
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
     }
 
     @Override
-    //设置了哪些请求归属SpringMVC处理
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
 }
+//public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
+//    @Override
+//    //加载SpringMVC容器配置
+//    protected WebApplicationContext createServletApplicationContext() {
+//        AnnotationConfigWebApplicationContext atx = new AnnotationConfigWebApplicationContext();//初始化一个空的AnnotationConfigWebApplicationContext容器
+//        atx.register(SpringMVCConfig.class);//将SpringMVCConfig.class注册到容器中，即加载具体的配置
+//        return atx;
+//    }
+//
+//    @Override
+//    //加载Spring容器的配置
+//    protected WebApplicationContext createRootApplicationContext() {
+//        AnnotationConfigWebApplicationContext atx = new AnnotationConfigWebApplicationContext();//初始化一个空的AnnotationConfigWebApplicationContext容器
+//        atx.register(SpringConfig.class);//将SpringMVCConfig.class注册到容器中，即加载具体的配置
+//        return atx;
+//    }
+//
+//    @Override
+//    //设置了哪些请求归属SpringMVC处理
+//    protected String[] getServletMappings() {
+//        return new String[]{"/"};
+//    }
+//}
 
 
