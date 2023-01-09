5990c0178db9ed0c20216734b3ee409558b3cb56
xgrt <2725751476@qq.com>
2023-01-09 11:08:04 +0800

    Initial commit

---
+++ SpringMVC04_request_param/src/main/java/com/xgrt/config/SpringMVCConfig.java
@@ -0,0 +1,9 @@
+package com.xgrt.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.xgrt.controller")
+public class SpringMVCConfig {
+}
---
+++ SpringMVC04_request_param/src/main/java/com/xgrt/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.xgrt.controller;
+
+import com.xgrt.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class UserController {
+    //普通参数
+    @RequestMapping("/commonParam")
+    @ResponseBody
+    public String commonParam(String name,int age){
+        System.out.println("普通参数传递：name ===> "+name);
+        System.out.println("普通参数传递：age ===> "+age);
+        return "{'module','common param'}";
+    }
+
+    //普通参数，请求参数与实参名不同
+    @RequestMapping("/commonParamDifferentName")
+    @ResponseBody
+    public String commonParamDifferentName(@RequestParam("name") String userName, int age) {
+        System.out.println("普通参数传递：name ===> "+userName);//结果：普通参数传递：name ===> null
+        System.out.println("普通参数传递：age ===> "+age);
+        return "{'module','common param different name'}";
+    }
+
+    //pojo参数
+    @RequestMapping("/pojoParam")
+    @ResponseBody
+    public String pojoParam(User user) {
+        System.out.println("pojo参数传递：user ===> "+user);
+        return "{'module','pojo param'}";
+    }
+
+    //嵌套pojo参数
+    @RequestMapping("/pojoContainPojoParam")
+    @ResponseBody
+    public String pojoContainPojoParam(User user) {
+        System.out.println("pojo嵌套参数传递：user ===> "+user);
+        return "{'module','pojo contain param'}";
+    }
+
+    //数组参数
+    @RequestMapping("/arrayParam")
+    @ResponseBody
+    public String arrayParam(String[] hobbyS){
+        System.out.println("数组参数传递：hobbyS ===> "+ Arrays.toString(hobbyS));
+        return "{'module','array param'}";
+    }
+
+    //集合参数
+    //实际上，不加@RequestParam，是把List和array当做POJO参数来用
+    @RequestMapping("/listParam")
+    @ResponseBody
+    public String listParam(@RequestParam List<String> hobbyS){
+        System.out.println("集合参数传递：hobbyS ===> "+ hobbyS);
+        return "{'module','list param'}";
+    }
+}
