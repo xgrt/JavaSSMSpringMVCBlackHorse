c2cb032ef40e7bd7a88ccd7801675c3aaec23e07
xgrt <2725751476@qq.com>
2023-01-22 16:24:52 +0800

    Initial commit

--- SpringMVC10_exception/src/main/java/com/xgrt/controller/Code.java
+++ SpringMVC10_exception/src/main/java/com/xgrt/controller/Code.java
@@ -10,4 +10,13 @@ public class Code {
     public static final Integer DELETE_ERROR = 20020;//删除失败的状态码
     public static final Integer UPDATE_ERROR = 20030;//修改失败的状态码
     public static final Integer GET_ERROR = 20040;//查询失败的状态码
+
+    public static final Integer SYSTEM_ERR = 50001;
+    public static final Integer SYSTEM_TIMEOUT_ERR = 50002;
+    public static final Integer SYSTEM_UNKNOWN_ERR = 59999;
+    //    ……
+    public static final Integer BUSINESS_ERR = 60002;
+    //    ……
+
+
 }
--- SpringMVC10_exception/src/main/java/com/xgrt/service/impl/BookServiceImpl.java
+++ SpringMVC10_exception/src/main/java/com/xgrt/service/impl/BookServiceImpl.java
@@ -1,7 +1,10 @@
 package com.xgrt.service.impl;
 
+import com.xgrt.controller.Code;
 import com.xgrt.dao.BookDao;
 import com.xgrt.domain.Book;
+import com.xgrt.exception.BusinessException;
+import com.xgrt.exception.SystemException;
 import com.xgrt.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,6 +41,22 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book selectById(Integer id) {
+
+        //将可能出现的异常进行包装。转换成自定义异常
+
+        /*if(id==1){//模拟不规范的输入操作
+            throw new BusinessException(Code.BUSINESS_ERR,"请不要用你的技术挑战我的耐心");
+        }
+
+        //最原始的方法
+        try {
+            int i=1/0;//模拟数据库异常
+        }catch (ArithmeticException e){
+            throw new SystemException(Code.SYSTEM_ERR,"服务访问超时，请重试");//转换成自定义异常
+        }*/
+
+
+
         return bookDao.selectById(id);
     }
 }
