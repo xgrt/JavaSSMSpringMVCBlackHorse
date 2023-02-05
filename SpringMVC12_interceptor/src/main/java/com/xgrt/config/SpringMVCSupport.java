eb1e6f3da300bad2a6ecce3975d3c838531bda07
xgrt <2725751476@qq.com>
2023-02-05 19:56:21 +0800

    Initial commit

--- SpringMVC12_interceptor/src/main/java/com/xgrt/config/SpringMVCSupport.java
+++ SpringMVC12_interceptor/src/main/java/com/xgrt/config/SpringMVCSupport.java
@@ -1,18 +1,28 @@
 package com.xgrt.config;
 
+import com.xgrt.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 public class SpringMVCSupport extends WebMvcConfigurationSupport {
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //当访问/page/???时候不要走MVC，走/page目录下的内容
         registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-        registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins/");
-
     }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        //addInterceptor()里面的参数是需要注入的拦截器
+        // addPathPatterns()里面的参数是需要拦截的路径，是可变个数的参数
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/books", "/books/*");
+    }
+
+
 }
--- SpringMVC12_interceptor/src/main/java/com/xgrt/controller/interceptor/ProjectInterceptor.java
+++ SpringMVC12_interceptor/src/main/java/com/xgrt/controller/interceptor/ProjectInterceptor.java
@@ -9,18 +9,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class ProjectInterceptor implements HandlerInterceptor {
     @Override
+    //在原始操作前执行的代码
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("preHandle……");
-        return true;
+        return true;//return false;会终止原始操作
     }
 
     @Override
+    //在原始操作后执行的代码
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("postHandle……");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
+    //在拦截操作结束后执行的代码，在postHandle执行的后面
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("afterCompletion……");
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
