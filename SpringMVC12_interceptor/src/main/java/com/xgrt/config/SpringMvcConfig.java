9acbb37334bc601bff1931f49b6474e703597247
xgrt <2725751476@qq.com>
2023-02-06 17:10:53 +0800

    Initial commit

--- SpringMVC12_interceptor/src/main/java/com/xgrt/config/SpringMvcConfig.java
+++ SpringMVC12_interceptor/src/main/java/com/xgrt/config/SpringMvcConfig.java
@@ -1,12 +1,24 @@
 package com.xgrt.config;
 
+import com.xgrt.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@ComponentScan({"com.xgrt.controller", "com.xgrt.config"})
+@ComponentScan({"com.xgrt.controller"})
 @EnableWebMvc
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+    //简化开发，无需扫描com.xgrt.config包作用相当于SpringMVCSupport类
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/books", "/books/*");
+    }
 }
